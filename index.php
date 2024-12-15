f078acc27c59608a562fe00b41d29448704fd6d6
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2024-12-15 19:13:28 -0300

    Criando e estilizando o form

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
     <header class="bg-stone-900">
 
-        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+        <nav class="mx-auto max-w-screen-lg flex justify-between py-4">
 
             <div class="font-bold text-xl tracking-wide">Book Wise</div>
 
@@ -35,8 +35,21 @@
 
     </header>
 
-    <main>
-        lista de livros
+    <main class="mx-auto max-w-screen-lg py-4 px-4 space-y-5">
+
+    <!-- <h1 class="mt-5 font-bold text-lg">Explorar</h1> -->
+        
+    <form class="w-full flex space-x-2 mt-6" action="">
+
+        <input type="text" name="pesquisar" id="" class="border-stone-800 rounded-md bg-stone-900 text-ml focus:outline-none px-2 py-1" placeholder="Pesquisar...">
+        <button type="submit">🔎</button>
+
+    </form>
+
+    <div>
+        lista final
+    </div>
+
     </main>
 
 </body>
