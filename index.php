0c277efc826b5dfed65d56807ed365fcd1d973e2
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2024-12-15 18:51:19 -0300

    estruturando o projeto

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookwise</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-stone-950 text-stone-400">
+
+    <header class="mx-auto max-w-screen-lg">
+
+        <nav class="bg-red-600 flex justify-between  px-8">
+
+            <div>Book Wise</div>
+
+            <ul>
+                <li>
+                    <a href="/">Explorar</a>
+                </li>
+                <li>
+                    <a href="/">Meus Livros</a>
+                </li>
+            </ul>
+
+            <ul>
+                <li>
+                    <a href="/">Fazer Login</a>
+                </li>
+            </ul>
+
+        </nav>
+
+    </header>
+
+    <main>
+        lista de livros
+    </main>
+
+</body>
+
+</html>
