0cadb1695e58c05a77833055ff842e3983e8ce7a
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2025-01-02 22:26:52 -0300

    Ajustes

--- routes.php
+++ routes.php
@@ -8,13 +8,15 @@ if (isset($_SERVER['PATH_INFO'])) {
     $controller = str_replace('/', '', $_SERVER['PATH_INFO']);
 }
 
+$uri = str_replace('/', '', parse_url($_SERVER['REQUEST_URI']));
+
+
 if(!file_exists("controllers/{$controller}.controller.php")) {
 
     http_response_code(404);
-
     echo "A página não existe!";
     die();
 }
 
-require "controllers/{$controller}.controller.php";
+// require "controllers/{$controller}.controller.php";
 
